b889fe07631247efee98f836d509bc2d413558fa
Aviv <avivbenrubi1@gmail.com>
2023-10-22 00:14:45 +0300

    Adding sausage by click to map

---
+++ src/components/Map/MapMarker/index.tsx
@@ -0,0 +1,64 @@
+import { MutableRefObject, useMemo, useRef } from "react";
+import L from "leaflet";
+import { Marker, Popup, useMapEvents } from "react-leaflet";
+import { Socket } from "socket.io-client";
+
+import Coordinate from "../../../interfaces/Coordinate";
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+  iconUrl: require("leaflet/dist/images/marker-icon.png"),
+  shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
+});
+
+function MapMarker() {
+  //const MapMarker = ({markerPos, socket}: {markerPos: Coordinate, socket: Socket}) => {
+  //const markerRef: MutableRefObject<any> = useRef(null);
+
+  // socket.on('getCoordinatesRT', (newCoord: Coordinate) => {
+  //     const marker: L.Marker = markerRef.current;
+  //     if (marker !== null) {
+  //         console.log(`got new coordinate: `, newCoord);
+  //         marker.setLatLng(L.latLng(newCoord.lat, newCoord.long));
+  //     }
+  // })
+  // socket.on('getNewCoordinate', (newCoord: Coordinate) => {
+  //     const marker: L.Marker = markerRef.current;
+  //     if (marker !== null) {
+  //         marker.setLatLng(L.latLng(newCoord.lat, newCoord.long));
+  //     }
+  // })
+
+  var myIcon = L.icon({
+    iconUrl: "https://cdn-icons-png.flaticon.com/512/786/786912.png",
+    iconSize: [70, 70], // size of the icon
+  });
+
+  const map = useMapEvents({
+    click: (e) => {
+      const newMarker: L.Marker = L.marker(e.latlng, { icon: myIcon }).addTo(
+        map
+      );
+      console.log(newMarker);
+      // socket.emit('getCoordinatesRT', ([{lat: markerPos.lat, lng: markerPos.long}, {lat: e.latlng.lat, lng: e.latlng.lng}]))
+    },
+  });
+
+  // const eventHandlers = useMemo(() => ({
+  //     click() {
+  //         socket.emit('getNewCoordinate');
+  //     },
+  // }), [socket]);
+
+  // return (
+  //     <Marker ref={markerRef} eventHandlers={eventHandlers} position={[markerPos.lat, markerPos.long]}>
+  //         <Popup>
+  //             A pretty CSS3 popup. <br /> Easily customizable.
+  //         </Popup>
+  //     </Marker>
+  // );
+
+  return null;
+}
+
+export default MapMarker;
--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,21 +1,28 @@
-'use client'
+"use client";
 
-import './Map.css'
-import 'leaflet/dist/leaflet.css'
+import "./Map.css";
+import "leaflet/dist/leaflet.css";
 
-import { LatLngExpression } from "leaflet"
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet"
+import { LatLngExpression } from "leaflet";
+import {
+  MapContainer,
+//   Marker,
+  Popup,
+  TileLayer,
+  useMapEvents,
+} from "react-leaflet";
+
+import { useState } from "react";
+
+import MapMarker from "./MapMarker";
 
 export default function Map() {
-    const position: LatLngExpression = [100, -0.09]
-
-    return (
-        <div className='map-wrapper'>
-            <MapContainer center={[31.681579, 35.007935]} zoom={8} className='map'>
-                <TileLayer
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-            </MapContainer>
-        </div>
-    )
+  return (
+    <div className="map-wrapper">
+      <MapContainer center={[31.681579, 35.007935]} zoom={8} className="map">
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <MapMarker/>
+      </MapContainer>
+    </div>
+  );
 }
