a94b147c5de0c259327648a222885b123352a73d
Tal Aneta <Talaneta01@gmail.com>
2023-10-25 20:40:08 +0300

    new props in the frame, props from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,51 @@ import Frame from "./components/MapComponent/DataComponent/frame";
 
 function App() {
   
+    type Data = {
+        longitude: any,
+        latitude: any,
+        altitude: any,
+        serial_number: any,
+        device_type: any
+    };
+    
+    const newData: Data[] = [
+        {
+            longitude:108,
+            latitude: 55,
+            altitude: 55,
+            serial_number: 55,
+            device_type:55
+        },
+        {
+            longitude:55555,
+            latitude: 55,
+            altitude: 55,
+            serial_number: 55,
+            device_type:55
+        },
+        {
+            longitude:55,
+            latitude: 55,
+            altitude: 55,
+            serial_number: 55,
+            device_type:55
+        },
+        {
+            longitude:55,
+            latitude: 55,
+            altitude: 55,
+            serial_number: 55,
+            device_type:55
+        }
+    ]
+
 
   return (
     <div className="App">
 
       <Grid container direction={"row"}>
-        <Grid item xs={3}><Frame /></Grid>
+        <Grid item xs={3}><Frame newData={newData} /></Grid>
         <Grid item xs={8}><MapComponent /></Grid>
 
 
--- src/components/MapComponent/DataComponent/frame.tsx
+++ src/components/MapComponent/DataComponent/frame.tsx
@@ -1,84 +1,59 @@
 import { Grid, Card } from "@mui/material";
 import { useState } from "react";
 import logo108 from "../../../assets/108.png"
-import { log } from "console";
 
 
-function Frame() {
-    //dataList:Array<Array<[string, any]>>
-    let data: Array<any> = [[
-        ["longitude", 55],
-        ["latitude", 55],
-        ["altitude", 55],
-        ["serial_number", 55],
-        ["device_type", 55], ["longitude", 55],
-        ["latitude", 55],
-        ["altitude", 55],
-        ["serial_number", 55],
-        ["device_type", 55]
-    ], [
-        ["longitude", 55],
-        ["latitude", 55],
-        ["altitude", 55],
-        ["serial_number", 55],
-        ["device_type", 55]
-    ], [
-        ["longitude", 55],
-        ["latitude", 55],
-        ["altitude", 55],
-        ["serial_number", 55],
-        ["device_type", 55]
-    ], [
-        ["longitude", 55],
-        ["latitude", 55],
-        ["altitude", 55],
-        ["serial_number", 55],
-        ["device_type", 55]
-    ], [
-        ["longitude", 55],
-        ["latitude", 55],
-        ["altitude", 55],
-        ["serial_number", 55],
-        ["device_type", 55]
-    ]]
+interface FrameProps {
+    newData: {
+        longitude: any;
+        latitude: any;
+        altitude: any;
+        serial_number: any;
+        device_type: any;
+    }[];
+}
+
+function Frame(props: FrameProps) {
+    const {
+        newData: [
+            {
+                longitude,
+                latitude,
+                altitude,
+                serial_number,
+                device_type
+            }
+        ]
+    } = props;
+
     return (
         <Grid spacing={2}>
             <img src={logo108} alt="108" style={{ margin: "0.5rem", height: "5vw" }} />
 
             <Grid style={{ display: "flex", margin: "2vw" }} direction="column">
-                {data && data.map((line) => (
+                {props.newData && props.newData.map((line) => (
                     <Card style={{ backgroundColor: "#80808057", marginBottom: "3vw", padding: "0.9rem", maxHeight: "10vw", overflowY: "auto" }}>
-                        {line.map((miniLine: any) => (
-                            <Grid container spacing={3} direction="row">
-                                <Grid item>{miniLine[0]}</Grid>
-                                <Grid item>{miniLine[1]}</Grid>
-                            </Grid>
-                        ))}
-                        {/* <Grid container spacing={3} direction="row">
-                            <Grid item>{line[0][0]}</Grid>
-                            <Grid item>{line[0][1]}</Grid>
-                        </Grid>
                         <Grid container spacing={3} direction="row">
                             <Grid item>longitude</Grid>
-                            <Grid item>5</Grid>
+                            <Grid item>{line?.longitude}</Grid>
                         </Grid>
                         <Grid container spacing={3} direction="row" >
                             <Grid item>latitude</Grid>
-                            <Grid item>55</Grid>
+                            <Grid item>{line?.latitude}</Grid>
 
                         </Grid>
                         <Grid container spacing={3} direction="row" >
                             <Grid item>altitude</Grid>
-                            <Grid item>55</Grid>
+                            <Grid item>{line?.altitude}</Grid>
 
                         </Grid><Grid container spacing={3} direction="row" >
                             <Grid item>serial_number</Grid>
-                            <Grid item>55</Grid>
+                            <Grid item>{line?.serial_number}</Grid>
 
                         </Grid><Grid container spacing={3} direction="row" >
                             <Grid item>device_type</Grid>
-                            <Grid item>55</Grid>
-                        </Grid> */}
+                            <Grid item>{line?.device_type}</Grid>
+                        </Grid>
 
                     </Card>
                 ))}
